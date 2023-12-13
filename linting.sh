00b2b52d14a930f77608f59985f1f8c5dee4cf89
Satyam-Kumar <Satyam.Kumar@coxautoinc.com>
2023-12-13 20:18:44 +0530

    Updated WF

--- linting.sh
+++ linting.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -l
-hostname --fqdn
-pwd
+echo "hostname --fqdn" && hostname --fqdn
+echo "pwd= $PWD"
 ls -al .
 ls -al /
-ls -al /home
+echo "ls -al /home" && ls -al /home
 ls -al /home/runner/work || true
-echo "Running ansible-lint on $1"
-ansible-lint -p $1
+# echo "Running ansible-lint on $1"
+# ansible-lint -p $1
+echo "Running ansible-lint on $PWD"
+ansible-lint -p .
