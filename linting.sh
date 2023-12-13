f7e26fcb41847ead64879ef0360401526282fcc5
Satyam-Kumar <Satyam.Kumar@coxautoinc.com>
2023-12-13 15:57:15 +0530

    Updated WF

--- linting.sh
+++ linting.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -l
 
-repo_url=$1
+git_repo=$1
+repo_url="git@github.com:$repo_url.git"
 echo "Running ansible-lint on $repo_url"
-git clone "git@github.com:$repo_url.git"
+git clone $repo_url
 repo_dir=${$repo_url#*/}
 ls $repo_dir
 ansible-lint -p $repo_dir
