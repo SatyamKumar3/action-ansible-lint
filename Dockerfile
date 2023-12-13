63abc6e774eb9436b9d502e2fec31563cf5110ac
Satyam-Kumar <Satyam.Kumar@coxautoinc.com>
2023-12-13 16:40:34 +0530

    Updated WF

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@
 FROM python:3
 # COPY linting.sh  /linting.sh
 COPY . .
+RUN echo "Repo name is $repo-name"
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 # RUN python3 -m pip install --upgrade pip
--- linting.sh
+++ linting.sh
@@ -1,9 +1,5 @@
 #!/bin/sh -l
 
-git_repo=$1
-repo_url="git@github.com:$git_repo.git"
-echo "Running ansible-lint on $repo_url"
-git clone $repo_url
-repo_dir=${$repo_url#*/}
-ls $repo_dir
-ansible-lint -p $repo_dir
+ls -al
+echo "Running ansible-lint on $1"
+ansible-lint -p $1
