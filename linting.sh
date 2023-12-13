de245268a1dd64f909693942d0931fd990ae99e3
Satyam-Kumar <Satyam.Kumar@coxautoinc.com>
2023-12-13 15:47:03 +0530

    Updated Dockerfile

--- linting.sh
+++ linting.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -l
 
-echo "Running ansible-lint on $1"
-ls $1
-ansible-lint -p $1
+repo_url = $1
+echo "Running ansible-lint on $repo_url"
+git clone "git@github.com:$repo_url.git"
+repo_dir = ${$repo_url#*/}
+ls $repo_dir
+ansible-lint -p $repo_dir
